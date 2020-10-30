71f54cfd5541d6f17e2638464326f561c1619f00
Azeez Abass <abassazeez6@gmail.com>
2020-10-30 02:13:49 -0600

    remove unneeded posts

--- project1/db/data.sql
+++ project1/db/data.sql
@@ -31,10 +31,3 @@ INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p1,"23-09-21","est. Maur
 INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p2,"01-09-21","pede. Cras vulputate velit","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.",u4);
 INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p3,"02-10-21","nisi a odio","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.",u6);
 INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p4,"21-10-21","semper auctor. Mauris vel turpis.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer",u8);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p5,"03-09-21","quis arcu vel quam","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur",u10);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p6,"12-09-21","eu sem. Pellentesque ut ipsum","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer",u12);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p7,"25-10-21","ultrices posuere cubilia Curae; Phasellus","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur",u14);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p8,"26-10-21","Cras pellentesque. Sed dictum.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur",u16);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p9,"05-10-21","purus ac tellus.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.",u18);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p10,"24-10-21","malesuada. Integer id magna et","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer",u20);
-INSERT INTO posts (pid,pdate,title,body,poster) VALUES (p11,"25-09-21","orci. Ut sagittis","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.",u22);
