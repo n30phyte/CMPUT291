035115a752991b06d7bdfff2b5302c9de4c163f4
Cynthia Li <sqli@ualberta.ca>
2020-10-31 16:03:06 -0600

    fixed to python 3.5

--- project1/StateManager.py
+++ project1/StateManager.py
@@ -3,15 +3,18 @@ from util import clear
 """
 A generic State Class
 """
+
+
 class State:
     stacks = True
     running = False
+
     def _enter(self):
         running = True
         clear()
         self.enter()
-        while (running):
-            self.process();
+        while running:
+            self.process()
 
     def enter(self): pass
 
@@ -23,26 +26,29 @@ class State:
 
     def exit(self): pass
 
+
 """
 A generic state machine class that will manage and transition between states
 """
+
+
 class StateManager:
-# keep track of the current state and state history
-# MAKE SURE TO SET THE START_STATE
-    states : dict = {}
-    stateStack : list = []
+    # keep track of the current state and state history
+    # MAKE SURE TO SET THE START_STATE
+    states = {}
+    stateStack = []
 
-    PREV_STATE : str = "_PREV_STATE_"
+    PREV_STATE = "_PREV_STATE_"
 
-    def addState(self, state: State, name :str):
+    def addState(self, state: State, name: str):
         self.states[name] = state
         state.manager = self
 
-    def start(self, startState : str):
+    def start(self, startState: str):
         self.stateStack.insert(0, startState)
         self.states[self.stateStack[0]].enter()
 
-    def changeState(self, nextStateName : str):
+    def changeState(self, nextStateName: str):
         if nextStateName == self.PREV_STATE:
             self.states[self.stateStack[0]]._exit()
             del self.stateStack[0]
@@ -54,7 +60,7 @@ class StateManager:
             nextState = self.states[nextStateName]
             # special case: state that uses the state stack, append to the stack
             if not nextState.stacks and nextStateName in self.stateStack:
-                while(self.stateStack[0] != nextStateName):
+                while (self.stateStack[0] != nextStateName):
                     self.states[self.stateStack[0]]._exit()
                     del self.stateStack[0]
             else:
