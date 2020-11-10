f88a3a96352f1b2343b22755b696b497483b23f6
Michael Kwok <michael.kwok00@gmail.com>
2020-11-09 18:32:51 -0700

    Add project 2 files

---
+++ project2/src/main/java/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+
+    public static void main(String[] args) {
+        if(args.length > 2) {
+            int portNumber = Integer.parseInt(args[1]);
+        }
+
+        System.out.println("UWU");
+    }
+}
