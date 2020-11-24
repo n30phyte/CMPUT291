476d451acae53c3bc45216c694052294db5e6037
Michael Kwok <michael.kwok00@gmail.com>
2020-11-24 13:42:20 -0700

    Threaded

--- project2/phase1.py
+++ project2/phase1.py
@@ -1,28 +1,14 @@
 import pymongo
 import json
 import sys
+import multiprocessing as mp
 
-if __name__ == "__main__":
-    port = 27017
-    if len(sys.argv) == 2:
-        port = int(sys.argv[1])
+
+def vote_func(port: int):
     client = pymongo.MongoClient("localhost", port)
     db = client["291db"]
 
     collection_list = db.list_collection_names()
-
-    # Check for existence and delete if exists
-    if "Posts" in collection_list:
-        post_collection = db.get_collection("Posts")
-        post_collection.drop()
-
-    post_collection = db.create_collection("Posts")
-
-    if "Tags" in collection_list:
-        tag_collection = db.get_collection("Tags")
-        tag_collection.drop()
-
-    tag_collection = db.create_collection("Tags")
 
     if "Votes" in collection_list:
         vote_collection = db.get_collection("Votes")
@@ -30,7 +16,51 @@ if __name__ == "__main__":
 
     vote_collection = db.create_collection("Votes")
 
-    # insert file data
+    with open("Votes.json") as votes:
+        votes_data = json.load(votes)
+        votes_data = votes_data["votes"]["row"]
+
+        vote_collection.insert_many(votes_data)
+        vote_collection.create_index("Id", unique=True, background=True)
+
+
+def tag_func(port: int):
+    client = pymongo.MongoClient("localhost", port)
+    db = client["291db"]
+
+    collection_list = db.list_collection_names()
+
+    if "Posts" in collection_list:
+        post_collection = db.get_collection("Posts")
+        post_collection.drop()
+
+    if "Tags" in collection_list:
+        tag_collection = db.get_collection("Tags")
+        tag_collection.drop()
+
+    tag_collection = db.create_collection("Tags")
+
+    with open("Tags.json") as tags:
+        tags_data = json.load(tags)
+        tags_data = tags_data["tags"]["row"]
+
+        tag_collection.insert_many(tags_data)
+
+        tag_collection.create_index("Id", unique=True, background=True)
+
+
+def post_func(port: int):
+    client = pymongo.MongoClient("localhost", port)
+    db = client["291db"]
+
+    collection_list = db.list_collection_names()
+
+    if "Posts" in collection_list:
+        post_collection = db.get_collection("Posts")
+        post_collection.drop()
+
+    post_collection = db.create_collection("Posts")
+
     with open("Posts.json") as post_file:
         posts_data = json.load(post_file)
         posts_data = posts_data["posts"]["row"]
@@ -43,17 +73,20 @@ if __name__ == "__main__":
             background=True,
         )
 
-    with open("Tags.json") as tags:
-        tags_data = json.load(tags)
-        tags_data = tags_data["tags"]["row"]
 
-        tag_collection.insert_many(tags_data)
+if __name__ == "__main__":
+    port = 27017
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
 
-        tag_collection.create_index("Id", unique=True, background=True)
+    post_thread = mp.Process(target=post_func, args=(port,))
+    vote_thread = mp.Process(target=vote_func, args=(port,))
+    tag_thread = mp.Process(target=tag_func, args=(port,))
 
-    with open("Votes.json") as votes:
-        votes_data = json.load(votes)
-        votes_data = votes_data["votes"]["row"]
+    post_thread.start()
+    vote_thread.start()
+    tag_thread.start()
 
-        vote_collection.insert_many(votes_data)
-        vote_collection.create_index("Id", unique=True, background=True)
+    vote_thread.join()
+    tag_thread.join()
+    post_thread.join()
